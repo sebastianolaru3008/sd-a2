cc58102473e60087141a65bd1ed770a1e6eda906
Sebastian Olaru <sebastian.olaru@rebeldot.com>
2022-04-03 21:45:24 +0300

    initial commit

---
+++ server/src/test/java/com/tabs/assignment2/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabs.assignment2.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
